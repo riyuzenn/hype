ed30390b49803aa8349e12238bc5f805a316c5d6
Zenqi <zenaiwellplayed@gmail.com>
2021-07-05 19:30:32 +0800

    Add errors

---
+++ hype/cli/errors.py
@@ -0,0 +1,15 @@
+
+
+class HypeException(Exception):
+    def __init__(self, msg=None):
+        if msg == None:
+            msg = "Something went wrong with the Hype CLI"
+
+        super().__init__(msg=msg)
+
+
+class TooMuchArguments(HypeException):
+    def __init__(self):
+        super().__init__("You passed too much arguments.")
+
+
