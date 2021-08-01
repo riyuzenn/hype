ba8f6054bf9d39a658cd3b6e7a77cfbcee9ef7ef
Zenqi <zenaiwellplayed@gmail.com>
2021-08-01 19:28:15 +0800

    Update color

--- hype/color.py
+++ hype/color.py
@@ -467,4 +467,11 @@ def print_color(
         if token_type == TOKEN_NEWLINE:
             seq.append(token_source)
 
-    print("".join(seq), sep=sep, end=end, file=file, flush=flush)
+    print(
+        
+        "".join(seq), 
+        sep=sep, 
+        end=end, 
+        file=file, 
+        flush=flush
+    )
